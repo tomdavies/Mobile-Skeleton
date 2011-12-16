9554dd0f3c91891176e29308d3466e845b99767b
Tom Davies <tom@tomdavies.net>
2011-12-16 10:45:02 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+  include($_SERVER['DOCUMENT_ROOT'].'/_includes/_head.inc');
+?>
+<?php /*per page head content in here*/;?>
+
+</head>
+
+<body>
+
+<header role="banner">
+  <h1>Enotria</h1>
+</header>
+
+<div class="content" role="main">
+
+</div>
+
+<?php include('_includes/_footer.inc');?>
